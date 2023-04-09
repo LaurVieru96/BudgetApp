09ab22fc6b32e4a853dace81c90f56802751b152
Laur <laurentiuvieru50@gmail.com>
2023-04-09 12:41:28 +0300

    update-create-budget

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,50 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard, { dashboardAction, dashboardLoader } from "./pages/Dashboard";
+import Error from "./pages/Error";
+
+//library
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+//layouts
+import Main, { mainLoader } from "./layouts/Main";
+
+// Actions
+import { logoutAction } from "./actions/logout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    loader: mainLoader,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+        loader: dashboardLoader,
+        action: dashboardAction,
+        errorElement: <Error />,
+      },
+      {
+        path: "logout",
+        action: logoutAction,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div className="App">hi!</div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default App;
+
+// #10 00:00
+
+// https://www.youtube.com/watch?v=qre4LgXyTPo&list=PL4cUxeGkcC9iNnY07bh_UPaRIQZcJfARY&index=10&ab_channel=TheNetNinja
